8612c86af86e4d4db6f06b316cc7dc104ffee99b
shaharmorag <shahar.sdm@gmail.com>
2021-12-18 22:01:22 +0200

    finished for first semester presentation

--- tracker.py
+++ tracker.py
@@ -1,26 +1,33 @@
 import cv2
 
-filename = "C://Users//shaha//Downloads//zoom_0.mp4"
-capture = cv2.VideoCapture(filename)
+file = "ISSshort.mp4"
+capture = cv2.VideoCapture(file)
 
-# tracker = cv2.TrackerMOSSE_create()
-tracker2 = cv2.TrackerCSRT_create()
+tracker = cv2.TrackerCSRT_create()
 success, image = capture.read()
 
 bbox = cv2.selectROI("Tracking", image, False)
-tracker2.init(image, bbox)
+tracker.init(image, bbox)
+
+def deawBox(image, bbox):
+    x, y, w, h = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
+    cv2.rectangle(image, (x, y), ((x+w), (y+h)), (225, 0, 225), 3, 1)
+
 
 while True:
     success, image = capture.read()
+    success, bbox = tracker.update(image)
+    print(bbox)
+    if success:
+        deawBox(image, bbox)
+    else:
+        pass
+
     cv2.imshow("Tracking", image)
+    key = cv2.waitKey(1)
 
-
-
-
-
+    if key == 27:
+        break
 
     if cv2.waitKey(1) & 0xff == ord('q'):
         break
-
-
-
